52ab7d45272b6a4eb28b62e15b995b69ffeaf2d3
krisztianAlt <alt.krisztian@gmail.com>
2020-12-24 00:05:11 +0100

    Get selected radio button (news agency) and country name

--- app/src/main/java/com/example/newsabouteuropeforandroid/CountryListAdapter.java
+++ app/src/main/java/com/example/newsabouteuropeforandroid/CountryListAdapter.java
@@ -1,9 +1,16 @@
 package com.example.newsabouteuropeforandroid;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.service.voice.VoiceInteractionSession;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewManager;
+import android.view.ViewParent;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,10 +18,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class CountryListAdapter extends RecyclerView.Adapter<CountryListAdapter.ViewHolder>{
     private CountryListData[] countryListData;
+    private RadioGroup newsAgencyRadioButtonGroup;
+    private Activity mainActivity;
 
     // RecyclerView recyclerView;
-    public CountryListAdapter(CountryListData[] listData) {
+    public CountryListAdapter(CountryListData[] listData, RadioGroup newsAgencyRadioButtonGroup, Activity main) {
         this.countryListData = listData;
+        this.newsAgencyRadioButtonGroup = newsAgencyRadioButtonGroup;
+        this.mainActivity = main;
     }
 
     @Override
@@ -27,17 +38,32 @@ public class CountryListAdapter extends RecyclerView.Adapter<CountryListAdapter.
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        final CountryListData myListData = countryListData[position];
+        final CountryListData countryData = countryListData[position];
         holder.textView.setText(countryListData[position].getCountryName());
         holder.imageView.setImageResource(countryListData[position].getImgId());
         holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(view.getContext(), "click on item: " + myListData.getCountryName (),Toast.LENGTH_LONG).show();
+
+                //Get selected radio button (selected news agency):
+                int radioButtonID = newsAgencyRadioButtonGroup.getCheckedRadioButtonId();
+                View radioButton = newsAgencyRadioButtonGroup.findViewById(radioButtonID);
+                int idx = newsAgencyRadioButtonGroup.indexOfChild(radioButton);
+                RadioButton r = (RadioButton) newsAgencyRadioButtonGroup.getChildAt(idx);
+                String selectedAgency = r.getText().toString();
+
+                Toast.makeText(view.getContext(), "Click country: " + countryData.getCountryName() + ", agency: " + selectedAgency, Toast.LENGTH_LONG).show();
+
+                // Start article listing:
+                ShowArticlesActivity.updateActivity(mainActivity);
+                Intent intent = new Intent(mainActivity.getApplicationContext(), ShowArticlesActivity.class);
+                intent.putExtra("selectedAgency", selectedAgency);
+                intent.putExtra("selectedCountry", countryData.getCountryName());
+                mainActivity.startActivity(intent);
+
             }
         });
     }
-
 
     @Override
     public int getItemCount() {
@@ -52,7 +78,7 @@ public class CountryListAdapter extends RecyclerView.Adapter<CountryListAdapter.
             super(itemView);
             this.textView = (TextView) itemView.findViewById(R.id.countryName);
             this.imageView = (ImageView) itemView.findViewById(R.id.countryFlag);
-            relativeLayout = (RelativeLayout)itemView.findViewById(R.id.relativeLayout);
+            relativeLayout = (RelativeLayout)itemView.findViewById(R.id.countryListRelativeLayout);
         }
     }
 }
