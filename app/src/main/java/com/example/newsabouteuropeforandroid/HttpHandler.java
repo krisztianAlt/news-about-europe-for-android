6ced44602b10ce19b9a593849af9c9eca314f5c4
krisztianAlt <alt.krisztian@gmail.com>
2020-12-26 00:39:31 +0100

    Implement sending article data request for News API

--- app/src/main/java/com/example/newsabouteuropeforandroid/HttpHandler.java
+++ app/src/main/java/com/example/newsabouteuropeforandroid/HttpHandler.java
@@ -24,36 +24,50 @@ public class HttpHandler {
     public HttpHandler() {
     }
 
-    /*public String makeServiceCall(String reqUrl,
-                                  String httpMethod) {
+    public String getArticlesFromAPI(String agencyName, String countryName) {
+        // Convert strings with spaces:
+        if (agencyName.contains(" ")){
+            agencyName = agencyName.replace(" ", "-");
+        }
+        if (countryName.contains(" ")){
+            countryName = countryName.replace(" ", "-");
+        }
+
+        // API request:
+        String apiKey = MainActivity.sharedPreferences.getString ("apiKey", "api key");
+        String urlString = "https://newsapi.org/v2/everything?q=\"" + countryName +
+                "\"&sources=" + agencyName +
+                "&sortBy=relevancy&apiKey=" + apiKey;
         String response = null;
         try {
-            String plainCredentials= MainActivity.clientEmail + ":" + MainActivity.clientPassword;
-            String base64Credentials = new String(android.util.Base64.encode(plainCredentials.getBytes(), android.util.Base64.NO_WRAP));
-
-            URL url = new URL(reqUrl);
+            // URL connection:
+            URL url = new URL(urlString);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod(httpMethod);
+            conn.setRequestMethod("GET");
             conn.setConnectTimeout(90000);
             conn.setUseCaches(false);
             conn.setRequestProperty("User-Agent", "MyAgent");
             conn.setReadTimeout(90000);
-            conn.setRequestProperty("Authorization", "Basic " + base64Credentials);
             conn.addRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            conn.setDoInput(true);
             conn.connect();
 
+            // Read the response:
             if (conn.getResponseCode() >= HttpURLConnection.HTTP_BAD_REQUEST){
                 InputStream errorStream = new BufferedInputStream(conn.getErrorStream());
-                String errorString = convertStreamToString(errorStream);
-                JSONObject error = new JSONObject(errorString);
+                response = convertStreamToString(errorStream);
                 errorStream.close();
-                return error.toString();
+                conn.disconnect();
+            } else {
+                try {
+                    InputStream in = new BufferedInputStream(conn.getInputStream());
+                    response = convertStreamToString(in);
+                    in.close();
+                } finally {
+                    conn.disconnect();
+                }
             }
-
-            // read the response
-            InputStream in = new BufferedInputStream(conn.getInputStream());
-            response = convertStreamToString(in);
-            in.close();
+            Log.e(TAG, response);
         } catch (MalformedURLException ex) {
             Log.e(TAG, "MalformedURLException: " + ex.getMessage());
         } catch (ProtocolException ex) {
@@ -66,59 +80,7 @@ public class HttpHandler {
         return response;
     }
 
-    public String makeServiceCall(String reqUrl,
-                                  String newUserName,
-                                  String newUserEmail) {
-        String response = null;
-
-        try {
-            Map<String, String> data = new HashMap<>();
-            data.put("name", newUserName);
-            data.put("email", newUserEmail);
-
-            JSONObject jsonObject = new JSONObject(data);
-
-            String plainCredentials= MainActivity.clientEmail + ":" + MainActivity.clientPassword;
-            String base64Credentials = new String(android.util.Base64.encode(plainCredentials.getBytes(), android.util.Base64.NO_WRAP));
-
-            URL url = new URL(reqUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setDoOutput(true);
-            conn.setReadTimeout(90000);
-            conn.setRequestProperty("Authorization", "Basic " + base64Credentials);
-            conn.addRequestProperty("Content-Type", "application/json");
-            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-            wr.write(jsonObject.toString());
-            wr.flush();
-
-            if (conn.getResponseCode() >= HttpURLConnection.HTTP_BAD_REQUEST ){
-                InputStream errorStream = new BufferedInputStream(conn.getErrorStream());
-                String errorString = convertStreamToString(errorStream);
-                JSONObject error = new JSONObject(errorString);
-                String status = (String) error.get("status");
-                Log.e(TAG, status);
-                errorStream.close();
-                return error.toString();
-            }
-
-            // read the response
-            InputStream in = new BufferedInputStream(conn.getInputStream());
-            response = convertStreamToString(in);
-            in.close();
-        } catch (MalformedURLException ex) {
-            Log.e(TAG, "MalformedURLException: " + ex.getMessage());
-        } catch (ProtocolException ex) {
-            Log.e(TAG, "ProtocolException: " + ex.getMessage());
-        } catch (IOException ex) {
-            Log.e(TAG, "IOException: " + ex.getMessage());
-        } catch (Exception ex) {
-            Log.e(TAG, "Exception: " + ex.getMessage());
-        }
-        return response;
-    }*/
-
     public String checkAPIKey(String newAPIKey) {
-        // String testAPIKey = "3ed83aa5f9564c91b68b08d31a3e7f29";
         String countryName = "hungary";
         String newsAgencyName = "reuters";
         String urlString = "https://newsapi.org/v2/everything?q=\"" + countryName +
